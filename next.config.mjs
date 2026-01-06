e8a80c1ee5dc30799e25c7e9c3f3f7adb6c82d22
rishab465 <yrishab479@gmail.com>
2026-01-06 23:45:18 +0530

    Switch to Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
-const repo = 'weatherapplications'
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	output: 'export',
-	trailingSlash: true,
-	images: {
-		unoptimized: true
-	},
-	basePath: isGithubActions ? `/${repo}` : '',
-	assetPrefix: isGithubActions ? `/${repo}/` : ''
-}
+const nextConfig = {}
 
 export default nextConfig;
